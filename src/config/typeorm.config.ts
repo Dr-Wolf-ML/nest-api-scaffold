11742ff7abc3bba064525e33db9866feabc859f5
Dr-Wolf-ML <wolfgangml@mac.com>
2023-06-09 13:10:28 +1000

    pre-prod commit

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -9,13 +9,67 @@ export class TypeOrmConfigService implements TypeOrmOptionsFactory {
     createTypeOrmOptions():
         | TypeOrmModuleOptions
         | Promise<TypeOrmModuleOptions> {
-        return {
-            type: 'sqlite',
-            synchronize: process.env.NODE_ENV === 'test' ? true : false,
-            database: this.configService.get<string>('DB_NAME'),
-            autoLoadEntities: true,
-            migrationsRun: process.env.NODE_ENV === 'test' ? true : false,
-            keepConnectionAlive: process.env.NODE_ENV === 'test' ? true : false,
-        };
+        if (process.env.NODE_ENV === 'development') {
+            return {
+                type: this.configService.get<any>('DB_TYPE'),
+                database: this.configService.get<string>('DB_NAME'),
+                synchronize: this.configService.get<boolean>('DB_SYNC'),
+                autoLoadEntities: true,
+                migrationsRun: this.configService.get<boolean>(
+                    'DB_MIGRATIONS_RUN'
+                ),
+                keepConnectionAlive: this.configService.get<boolean>(
+                    'KEEP_CONNECTION_ALIVE'
+                ),
+            };
+        }
+
+        if (process.env.NODE_ENV === 'test') {
+            return {
+                type: this.configService.get<any>('DB_TYPE'),
+                database: this.configService.get<string>('DB_NAME'),
+                synchronize: this.configService.get<boolean>('DB_SYNC'),
+                autoLoadEntities: true,
+                migrationsRun: this.configService.get<boolean>(
+                    'DB_MIGRATIONS_RUN'
+                ),
+                keepConnectionAlive: this.configService.get<boolean>(
+                    'KEEP_CONNECTION_ALIVE'
+                ),
+                // type: 'sqlite',
+                // synchronize: process.env.NODE_ENV === 'test' ? true : false,
+                // database: this.configService.get<string>('DB_NAME'),
+                // autoLoadEntities: true,
+                // migrationsRun: process.env.NODE_ENV === 'test' ? true : false,
+                // keepConnectionAlive:
+                //     process.env.NODE_ENV === 'test' ? true : false,
+            };
+        }
+
+        if (process.env.NODE_ENV === 'production') {
+            return {
+                type: this.configService.get<any>('DB_TYPE'),
+
+                database: this.configService.get<string>('DB_NAME'),
+                synchronize: this.configService.get<boolean>('DB_SYNC'),
+                autoLoadEntities: true,
+                migrationsRun: this.configService.get<boolean>(
+                    'DB_MIGRATIONS_RUN'
+                ),
+                ssl: {
+                    rejectUnauthorized: this.configService.get<boolean>(
+                        'REJECT_UNAUTHORIZED'
+                    ),
+                },
+            };
+        }
     }
 }
+
+//? Legacy:
+// type: 'sqlite',
+// synchronize: process.env.NODE_ENV === 'test' ? true : false,
+// database: this.configService.get<string>('DB_NAME'),
+// autoLoadEntities: true,
+// migrationsRun: process.env.NODE_ENV === 'test' ? true : false,
+// keepConnectionAlive: process.env.NODE_ENV === 'test' ? true : false,
